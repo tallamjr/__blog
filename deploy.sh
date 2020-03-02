33d347b41280a18a450de14a12b1bd08184d9f83
Tarek Allam <t.allam.jr@gmail.com>
2020-03-02 20:50:36 +0000

    Restructure deploy script
    Include descriptive comments
    
    	modified:   deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+# Serve website at http://localhost:1313/blog/ and send to bg
 hugo serve &
-
+# Run link checker
 muffet --verbose http://localhost:1313/blog/
+# Grab exit code from 'muffet'
 exitCode=$?
+# Kill background 'hugo serve' process
+kill $(ps aux | grep "hugo serve" | grep -v "grep" | awk '{print $2}')
+# If 'muffet' was successfull, then compile html and deply to gh-pages
 if [[ $exitCode == 0 ]]; then
     rm -rf public/* && hugo && cd public && git add --all && git commit -m "Publishing to gh-pages"
     git push --all
 fi
-
-kill $(ps aux | grep "hugo serve" | grep -v "grep" | awk '{print $2}')
