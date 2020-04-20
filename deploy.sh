f35a9d42d45742fb27b0a8e8b6152478048a003b
Tarek Allam <t.allam.jr@gmail.com>
2020-04-20 12:25:20 +0100

    Remove muffet checks
    Unable to run checks all in one go. Simple deployment preferred for now
    
    REF: https://github.com/tallamjr/blog/issues/2#issuecomment-593522073
    
    	modified:   deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
-
-# Serve website at http://localhost:1313/blog/ and send to bg
-hugo serve &
-# Run link checker
-muffet --verbose http://localhost:1313/blog/
-# Grab exit code from 'muffet'
-exitCode=$?
-# Kill background 'hugo serve' process
-kill $(ps aux | grep "hugo serve" | grep -v "grep" | awk '{print $2}')
-# If 'muffet' was successfull, then compile html and deply to gh-pages
-if [[ $exitCode == 0 ]]; then
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+if [[ $BRANCH == "master" ]]; then
     rm -rf public/* && hugo && cd public && git add --all && git commit -m "Publishing to gh-pages"
     git push --all
+    cd ..
 fi
